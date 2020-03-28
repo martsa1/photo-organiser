e145d60dd650f95ec4fd4d2f8aab4e9e79b8d36d
Sam Martin <martsa@f-secure.com>
2020-03-28 19:53:56 +0000

    Add some additional date stamp handling routines

--- organiser/image_metadata.py
+++ organiser/image_metadata.py
@@ -1,8 +1,16 @@
 """Module collecting functions related to image metadata."""
 
 import exifread
-from typing import Dict
+from typing import Dict, List
 import io
+import logging
+import pendulum
+
+from organiser.types import FileTarget
+
+
+LOG = logging.getLogger(__name__)
+
 
 def get_file_meta(file_content: bytes) -> Dict[str, exifread.classes.IfdTag]:
     """Retrieve metadata for the provided file data."""
@@ -23,7 +31,44 @@ def get_file_meta(file_content: bytes) -> Dict[str, exifread.classes.IfdTag]:
 
     return file_exif_data
 
-def temp_load_from_filepath(target: str) -> bytes:
-    """Temporary convenience function, returns a file's contents from a file path as bytes."""
-    with open(target, "rb") as file_handle:
-        return file_handle.read()
+
+def parse_image_meta_for_file_target(target: FileTarget) -> FileTarget:
+    """Wrap get_file_meta for use within RX pipelines."""
+    if target.file_contents:
+        target.image_metadata = get_file_meta(target.file_contents)
+
+    return target
+
+
+def identify_image_datestamp(target: FileTarget) -> FileTarget:
+    """Attempt to process image metadata for FileTarget, setting FileTarget.datestamp on success."""
+    known_date_fields = ("EXIF DateTimeDigitized", "EXIF DateTimeOriginal", "Image DateTime")
+
+    possible_datestamps: List[str] = []
+    for field in known_date_fields:
+        target_date = target.image_metadata.get(field, None)
+        if target_date:
+            possible_datestamps.append(target_date.printable)
+
+    parsed_datestamps: List[pendulum.DateTime] = []
+    for datestamp in possible_datestamps:
+        try:
+            parsed_datestamps.append(pendulum.parse(datestamp))
+        except pendulum.exceptions.PendulumException:
+            LOG.debug("Unable to parse %s into a native DateTime.", datestamp)
+            pass
+
+    if not parsed_datestamps:
+        LOG.info("Unable to identify image datestamp")
+        return target
+
+    if len(parsed_datestamps) == 1:
+        LOG.debug("Date fields seem to agree with one another")
+        target.datestamp = parsed_datestamps[0]
+
+    else:
+        # Take the newest one - This is somewhat arbitrary...
+        LOG.debug("Date fields did not agree with one another: %s", parsed_datestamps)
+        target.datestamp = max(parsed_datestamps)
+
+    return target
