26024630a130c727b3689b01ec0318862bf33dbe
Sam Martin <martsa@f-secure.com>
2020-03-28 19:55:02 +0000

    Re-Write application to leverage RX, rather than a home baked async implementation

--- organiser/main.py
+++ organiser/main.py
@@ -1,176 +1,74 @@
 #!/usr/bin/env python
-import asyncio
 import logging
-import os
-import re
 
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-from hashlib import md5
-from pprint import pprint
-from typing import AsyncGenerator, AnyStr, Dict, Any, Tuple, Mapping, Union, cast, AsyncIterable
-
-import exifread
 import fire
+import rx
+from rx import operators
 
-in_progress_images: Dict[Tuple[str, str], asyncio.Future] = {}
-image_metadata: Dict[str, Dict[str, Any]] = {}
+from organiser import file_listing as fl
+from organiser import filename_calculations as fc
+from organiser import image_metadata as im
+from organiser.types import FileTarget
 
 LOG = logging.getLogger(__name__)
 
 
-async def file_listing(
-        base_dir: str = None,
-        filename_filter: str = '',
-        #  ) -> AsyncGenerator[str, None]:
-) -> AsyncIterable[str]:
-    """Return a files list.
+def main(base_dir: str = "", storage_dir: str = "", copy_only: bool = False) -> None:
+    """Organise image files from one location to another.
+
+    This application allows you to specify a base directory from which to
+    recursively search for image files, and to organise those files, based on
+    the date they were taken,into a collection of year and month folders.  The
+    application will respect albums which already exist based on the presence
+    of an album name.
+
+    By setting the --copy-only flag, this application will copy, rather than
+    the default move, files when organising them.
+
+    Arguments:
+        base_dir: The location from which the application should search for
+            image files.
+        storage_dir: The location from which the application should create the
+            archive of organised files.
+        copy_only: A flag to request that we make copies of files, rather than
+            moving them.
     """
-    if not base_dir:
-        base_dir = os.path.abspath(__file__)
-
-    for directory, _, filenames in os.walk(base_dir):
-        for filename in filenames:
-            if file_listing is not None and re.search(filename_filter, filename):
-                yield os.path.normpath(os.path.join(directory, filename))
-
-
-def get_file_meta(file_path: AnyStr) -> Dict[str, Any]:
-    """Retrieve metadata for the provided file path."""
-    if not file_path:
-        raise ValueError("Cannot operate on provided argument: {}".format(file_path))
-
-    #  print('Retrieving EXIF data for {}'.format(file_path))
-    with open(file_path, 'rb') as file_handle:
-        file_exif_data = exifread.process_file(file_handle)
-        file_exif_data = {
-            tag: file_exif_data[tag]
-            for tag in file_exif_data
-            if tag not in (
-                'EXIF MakerNote',
-                'JPEGThumbnail',
-                'TIFFThumbnail',
-            )
-        }
-
-    return file_exif_data
-    #  for tag in file_exif_data.keys():
-    #     if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-    #         print("Key: {}, value {}".format(tag, a[tag]))
-
-
-def get_file_hash(file_path: AnyStr) -> str:
-    """Calculate the MD5 hash of the provided file path."""
-
-    #  print('Hashing {}'.format(file_path))
-    chunk_size = 65 * 1024  # 65KiB.
-    md5_hash = md5()
-
-    with open(file_path, 'rb') as file_handle:
-        data = file_handle.read(chunk_size)
-        while data:
-            md5_hash.update(data)
-            data = file_handle.read(chunk_size)
-
-    #  print('MD5 for {}: {}'.format(file_path, md5_hash.hexdigest()))
-    return md5_hash.hexdigest()
-
-
-def process_executor_exif(future: asyncio.Future, executor_id: Tuple[str, str]) -> Dict:
-    """Print the results of the executor future"""
-    result: Dict[str, Union[str, int, float, Any]] = future.result()
-    #  print(executor_id, result.get('Image DateTime', None))
-
-    processed_file = executor_id[0]
-    if processed_file in image_metadata:
-        image_metadata[processed_file]['exif'] = result
-    else:
-        image_metadata[processed_file] = {'exif': result}
-
-    del in_progress_images[executor_id]
-
-    return result
-
-
-def process_executor_hash(future: asyncio.Future, executor_id: Tuple[str, str]) -> None:
-    """Print the results of the executor future"""
-    result = future.result()
-    #  print(executor_id, result)
-
-    processed_file = executor_id[0]
-    if processed_file in image_metadata:
-        image_metadata[processed_file]['md5'] = result
-    else:
-        image_metadata[processed_file] = {'md5': result}
-
-    del in_progress_images[executor_id]
-
-
-async def main(base_dir: str = '', storage_dir: str = ''):
-    """Launch into the primary application from here.
-    """
-    loop = asyncio.get_running_loop()
-    sync_thread_pool = ThreadPoolExecutor(max_workers=3)
-
-    if not os.path.isdir(base_dir):
-        base_dir = os.path.dirname(__file__)
-
-    # If we don't provide an option, default to organising the directory we are scanning.
-    if not os.path.isdir(storage_dir):
-        storage_dir = base_dir
-
-    async for file_path in file_listing(base_dir, r'.*(?:jpg|JPG|JPEG|jpeg)$'):
-        #  print(file_path)
-
-        img_exif: asyncio.Future = cast(
-            asyncio.Future, loop.run_in_executor(sync_thread_pool, get_file_meta, file_path)
-        )
-        img_md5: asyncio.Future = cast(
-            asyncio.Future, loop.run_in_executor(sync_thread_pool, get_file_hash, file_path)
-        )
-
-        #  print('scheduling exif')
-        img_exif.add_done_callback(partial(process_executor_exif, executor_id=(file_path, 'exif')))
-        #  print('exif processing scheduled for {}'.format(file_path))
-
-        #  print('scheduling md5')
-        img_md5.add_done_callback(partial(process_executor_hash, executor_id=(file_path, 'hash')))
-        #  print('md5 processing scheduled for {}'.format(file_path))
-
-        in_progress_images[cast(str, file_path), 'exif'] = img_exif
-        in_progress_images[cast(str, file_path), 'hash'] = img_md5
-
-    while in_progress_images:
-        await asyncio.sleep(0.5)
-        print('{} images to process'.format(len(in_progress_images.keys())))
-
-    pprint(
-        {
-            image_path: {
-                'exif': image_metadata[image_path]['exif'].get('EXIF DateTimeOriginal', 'Unknown'),
-                'md5': image_metadata[image_path].get('md5', 'No MD5?'),
-            }
-            for image_path in image_metadata
-        }
+    file_listing = rx.from_iterable(
+        fl.file_listing_iterator(base_dir, r'.*(?:jpg|JPG|JPEG|jpeg)$'),
     )
-    #  image_results = []
-    #  for result in asyncio.as_completed(image_metadata, loop=loop):
-    #      image_results.append(result.result())
 
-    #  #  print(image_results)
-    #  print(image_results)
+    hashed_files = file_listing.pipe(
+        operators.map(fl.load_file_contents),
+        operators.map(fl.sha256_file),
+        operators.map(fl.encode_shasum),
+    )
 
+    files_with_metadata = hashed_files.pipe(
+        operators.map(im.parse_image_meta_for_file_target),
+        operators.map(im.identify_image_datestamp),
+    )
 
-class CLI:
+    files_with_move_path = files_with_metadata.pipe(
+        operators.map(lambda target: fc.identify_photo_move_path("", target)),
+        operators.map(lambda target: target.clear_contents_data()),
+    )
 
-    @staticmethod
-    def organise(base_dir: str, storage_dir: str = ''):
-        """Process images from the provided base_dir"""
-        asyncio.run(main(base_dir, storage_dir))
+    def printer(target: FileTarget) -> str:
+        """Temp printer closure."""
+        return (
+            f"File Target:\n"
+            f"   Path: {target.file_path}\n"
+            f"   Hash: {target.encoded_hash}\n"
+            f"   Target Path: {target.target_move_path}\n"
+            f"   Image taken at: {target.image_metadata.get('EXIF DateTimeOriginal', 'Unknown')}"
+        )
 
-    def __call__(self):
-        asyncio.run(main())
+    files_with_move_path.subscribe(
+        on_next=lambda target: print(printer(target)),
+        on_error=lambda err: print(err),
+        on_completed=lambda: print("Done"),
+    )
 
 
 if __name__ == '__main__':
-    fire.Fire(CLI)
+    fire.Fire(main)
